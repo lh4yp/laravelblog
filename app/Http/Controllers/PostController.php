871f5102354e1442272f2fa6f190a6a7dd03bf04
lh4yp <39228092+lh4yp@users.noreply.github.com>
2018-06-27 20:32:08 +0800

    Add files via upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //列表页
+    public function index(){
+        return view('post/index');
+    }
+
+    //详情
+    public function view(){
+
+    }
+
+    //创建文章页
+    public function create(){
+
+    }
+
+    //保存文章
+    public function save(){
+
+    }
+
+    //编辑文章页
+    public function edit(){
+
+    }
+
+    //修改文章
+    public function update(){
+
+    }
+
+    //删除文章
+    public function delete(){
+
+    }
+}
